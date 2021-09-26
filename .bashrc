2f998497d15ff6b70c76fd6c8f52fb7e966e8db0
MarcZoon <marc@famzoon.nl>
2021-09-26 10:34:21 +0200

    added install and uninstall scripts

--- .bashrc
+++ .bashrc
@@ -1,3 +1,5 @@
+# .bashrc from https://gitlab.com/m3361/dotfiles
+
 # ~/.bashrc: executed by bash(1) for non-login shells.
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Find all .files
+# If original file exists, back it up to {file}.dtbak
+# Symlink new .file
+
+for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+    if [ -e ~/$file ]; then
+        mv -f ~/$file{,.dtbak}
+    fi
+    ln -s $PWD/$file ~/$file
+done
+
+echo "Installed!"
