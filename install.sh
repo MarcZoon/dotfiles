614f14793421ea350f986374548dea05575ececa
Marc Zoon <marc@famzoon.nl>
2022-12-09 18:59:14 +0100

    set zsh as default. auto start tmux

--- install.sh
+++ install.sh
@@ -18,6 +18,20 @@
 
 # echo "Backed up original files and created symlinks."
 
+# if not installed, install tmux, zshell
+if command -v zsh > /dev/null; then
+    echo "zsh already installed"
+else
+    echo "installing zsh"
+    sudo apt install zsh -y
+fi
+if command -v tmux > /dev/null; then
+    echo "tmux already installed"
+else
+    echo "installing tmux"
+    sudo apt install tmux -y
+fi
+
 # symlink oh-my-posh
 if [ ! -L /usr/local/bin/oh-my-posh ]; then
     echo "Create oh-my-posh symlink"
@@ -29,3 +43,12 @@ if [ ! -L ~/.poshthemes ]; then
     echo "Create .poshthemes symlink"
     ln -s $PWD/.poshthemes/ ~/.poshthemes
 fi
+
+# symlink other dotfiles
+for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+    ln -s $PWD/$file ~/$file
+done
+
+# set zsh as default shell
+echo "Set zsh as default shell"
+chsh -s /usr/bin/zsh
