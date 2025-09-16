d35a6471fe723f3272bfac47655a9ab42f09c5b8
Marc Zoon <marc.zoon@wortell.nl>
2025-09-16 14:43:43 +0200

    replace copilot.vim with copilot.lua

--- home/dot_config/nvim/lua/plugins/github_copilot.lua
+++ home/dot_config/nvim/lua/plugins/github_copilot.lua
@@ -1,3 +1,23 @@
 return {
-	"github/copilot.vim",
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+			},
+			panel = {
+				enabled = false,
+			},
+			filetypes = {
+				javascript = true,
+				lua = true,
+				python = true,
+				typescript = true,
+				["*"] = false,
+			},
+		})
+	end,
 }
