bf98b7ac109ee54b65397ef52a06a3ce84f305e3
Marc Zoon <marc.zoon@wortell.nl>
2025-09-17 11:47:41 +0200

    rename file and add should_attach function

--- home/dot_config/nvim/lua/plugins/copilot.lua
+++ home/dot_config/nvim/lua/plugins/copilot.lua
@@ -18,6 +18,15 @@ return {
 				typescript = true,
 				["*"] = false,
 			},
+			should_attach = function(_, bufname)
+				if string.match(bufname, "%.env$") then
+					return false
+				end
+				if string.match(bufname, "^env$") then
+					return false
+				end
+				return true
+			end,
 		})
 	end,
 }
